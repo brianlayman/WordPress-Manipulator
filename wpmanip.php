a3f59f512f78b8a9499a926cbdda28fdee11db62
Brian Layman <brian@ehermitsinc.com>
2011-09-28 10:50:13 -0400

    Added a time out refresh to allow the completion of the script. Fixed some styles and standards Commented out two lines I'd intended as comments only.

--- wpmanip.php
+++ wpmanip.php
@@ -35,8 +35,8 @@ if ( !defined('TARGET_URL' ) ) define( 'TARGET_URL', 'http://example.com' );
 if ( !defined('LOGIN_PATH' ) ) define( 'LOGIN_PATH', '/wp-login.php' );
 if ( !defined('WP_USER_NAME' ) ) define( 'WP_USER_NAME', 'admin' );
 if ( !defined('WP_USER_PASSWORD' ) ) define( 'WP_USER_PASSWORD', 'changeme' );
-if ( !defined('BLOCK_SIZE' ) ) define( 'BLOCK_SIZE', '5' );
-
+if ( !defined('BLOCK_SIZE' ) ) define( 'BLOCK_SIZE', '5' ); // The number of blogs I request in this example
+if ( !defined('ONGOING_TIME_OUT' ) ) define( 'ONGOING_TIME_OUT', '30' ); // The time in seconds allowed to get through BLOCK_SIZE actions
 
 // Integrate with the WordPress environment
 // Not needed if you aren't calling any WordPress functions
@@ -50,16 +50,13 @@ class wpManipApi {
 
 	
 	// A private constructor; prevents direct creation of object
-	private function __construct() 
-	{
+	private function __construct() {
 		$this->ckfile = tempnam( "/tmp", "cookie_wpManip_" );
 		$this->curl = curl_init();	   
 	}
 
-	public static function getInstance() 
-	{ 
-		if ( !self::$m_pInstance ) 
-		{ 
+	public static function getInstance() { 
+		if ( !self::$m_pInstance ) { 
 			self::$m_pInstance = new wpManipApi(); 
 		} 
 
@@ -102,6 +99,9 @@ $pageInfo = $pwpManipApi->_authorize(WP_USER_NAME, WP_USER_PASSWORD );
 $n = 1;
 
 while ( true ) {
+	// At the start of the loop set a time out in case we slam the server accidentally, but restart the timer each loop so that we can complete the action
+	set_time_limit(ONGOING_TIME_OUT);
+	
 	// This example assumes WordPress Multisite.  Scrap this whole loop for a single site application.
 	$blogs = $wpdb->get_results( "SELECT * FROM {$wpdb->blogs} WHERE spam = '0' AND deleted = '0' AND archived = '0' ORDER BY registered DESC LIMIT {$n}, " . BLOCK_SIZE, ARRAY_A );
 	if ( empty( $blogs ) ) {
@@ -111,9 +111,9 @@ while ( true ) {
 		$siteurl = get_blog_option( $details['blog_id'], 'siteurl' );
 		$pInfo = $pwpManipApi->hit_url( $siteurl . '/wp-admin' );
 		// You might want to display something here. You could echo out pInfo for debugging purposes
-		echo $siteurl . '/wp-admin' . '<br />' ;
+		// echo $siteurl . '/wp-admin' . '<br />' ;
 	}
 	$n += BLOCK_SIZE;
 	// Add a die() here to halt the run short for testing purposes
-	die();
+	// die();
 }
